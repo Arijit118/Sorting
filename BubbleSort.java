adae0279be44b945c2fe9b3ede3b4137332029b0
Arijit Roy <arijitroy042017@gmail.com>
2022-10-23 12:40:05 +0530

    add modified bubble sort

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,29 +1,52 @@
 package Sorting;
 
 public class BubbleSort {
-    //Time complexity O(n2)
+    // Time complexity O(n2)
     public static void bubbleSort(int nums[]) {
         int n = nums.length;
-        for(int turn= 0; turn<=n-2; turn++) {
-            for(int j= 0; j<=n-2-turn; j++) {
-                if(nums[j] > nums[j+1]) {
-                    //swap
+        for (int turn = 0; turn < n - 1; turn++) {
+            for (int j = 0; j < n - 1 - turn; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    // swap
                     int temp = nums[j];
-                    nums[j] = nums[j+1];
-                    nums[j+1] = temp;
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
                 }
             }
         }
     }
+
+    public static void modifiedBubbleSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            boolean swapped = false;
+
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (swapped == false) {
+                break;
+            }
+        }
+    }
+
     public static void printArr(int nums[]) {
-        for(int i=0; i<nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             System.out.print(nums[i] + " ");
         }
         System.out.println();
     }
+
     public static void main(String args[]) {
-        int nums[] = {5, 4, 1, 3, 2};
-        bubbleSort(nums);
+        int nums[] = { 5, 4, 1, 3, 2 };
+        // bubbleSort(nums);
+        // printArr(nums);
+        modifiedBubbleSort(nums);
         printArr(nums);
+
     }
 }
